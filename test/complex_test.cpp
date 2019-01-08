3c58f43ae2b76c64232e9a996ce62839020d509c
Johan Jönsson <johanjoensson@gmail.com>
2019-01-08 13:44:56 +0100

    Fixed the test

--- test/complex_test.cpp
+++ test/complex_test.cpp
@@ -5,6 +5,6 @@ TEST(ComplexTest, TestEqual)
 {
 	double re = 5.0, im = 1.3;
 	GSL::Complex a(re, im);
-	EXPECT_EQ(a.re, re) << "Real parts do not match! " << a.re << " != " << re;
-	EXPECT_EQ(a.im, im) << "Imaginary parts do not match " << a.im << " != " << im;
+	EXPECT_EQ(a.re(), re) << "Real parts do not match! " << a.re() << " != " << re;
+	EXPECT_EQ(a.im(), im) << "Imaginary parts do not match " << a.im() << " != " << im;
 }
