55c80661645a1294b1d68e2b0f8755fb4e001188
Johan Jönsson <johanjoensson@gmail.com>
2019-01-08 13:26:23 +0100

    Might as well start a new branch just for writing tests

--- test/complex_test.cpp
+++ test/complex_test.cpp
@@ -5,6 +5,24 @@ TEST(ComplexTest, TestEqual)
 {
 	double re = 5.0, im = 1.3;
 	GSL::Complex a(re, im);
-	EXPECT_EQ(a.re, re) << "Real parts do not match! " << a.re << " != " << re;
-	EXPECT_EQ(a.im, im) << "Imaginary parts do not match " << a.im << " != " << im;
+	EXPECT_EQ(a.re(), re) << "Real parts do not match! " << a.re() << " != " << re;
+	EXPECT_EQ(a.im(), im) << "Imaginary parts do not match " << a.im() << " != " << im;
+}
+
+TEST(ComplexTest, TestNotEqual)
+{
+	double re = 5.0, im = 1.3;
+	GSL::Complex a(re, im);
+	EXPECT_NE(a.re(), re - 1) << "Real parts do match! " << a.re() << " == " << re - 1;
+	EXPECT_NE(a.im(), im + 2) << "Imaginary parts do match " << a.im() << " == " << im + 2;
+}
+
+TEST(ComplexTest, TestAssign)
+{
+	double re = 4.3, im = 5;
+	GSL::Complex a, b = GSL::Complex(re, im);
+	a = b;
+	EXPECT_EQ(a.re(), re) << "Real parts do not match! " << a.re() << " != " << re;
+	EXPECT_EQ(a.im(), im) << "Imaginary parts do not match " << a.im() << " != " << im;
+
 }
